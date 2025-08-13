8a30c508e1caaa0e61f0e4c787c8e9cf4cc39a31
bara baraya selatan <barabarayaselatan96@gmail.com>
2025-08-13 23:29:05 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="px-8 tex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,21 +17,7 @@ export default function LoginPage() {
         </div>
         <LoginForm />
         <p className="px-8 text-center text-sm text-muted-foreground">
-          By clicking continue, you agree to our{" "}
-          <a
-            href="/terms"
-            className="underline underline-offset-4 hover:text-primary"
-          >
-            Terms of Service
-          </a>{" "}
-          and{" "}
-          <a
-            href="/privacy"
-            className="underline underline-offset-4 hover:text-primary"
-          >
-            Privacy Policy
-          </a>
-          .
+          Copyright 2025 Kel. Bara Baraya Selatan
         </p>
       </div>
     </div>
