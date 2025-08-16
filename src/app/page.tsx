1fd83f6aede9784865e05d93ef6ff2a6798a3eaf
bara baraya selatan <barabarayaselatan96@gmail.com>
2025-08-16 02:25:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function LoginPage() {
     <div className="relative flex min-h-screen flex-col items-center justify-center bg-background">
       <div className="mx-auto flex w-full max-w-[350px] flex-col justify-center space-y-6">
         <Image 
-          src="https://images.unsplash.com/photo-1755310672242-80ce88b024b9?w=1280&h=320&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MXx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.unsplash.com/photo-1755310672242-80ce88b024b9?w=1500&h=750&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MXx8fGVufDB8fHx8fA%3D%3D"
           alt="Banner"
           width={350}
-          height={88}
+          height={175}
           className="rounded-lg object-cover mb-4"
           data-ai-hint="nature landscape"
         />
@@ -20,7 +20,7 @@ export default function LoginPage() {
           <h1 className="text-2xl font-semibold tracking-tight font-headline text-secondary">
             SampahKU
           </h1>
-          <p className="text-sm text-secondary">
+          <p className="text-sm text-blue-500">
             Aplikasi Pembayaran Iuran Sampah
           </p>
         </div>
