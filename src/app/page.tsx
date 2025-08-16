0cd268c4ebee6853be766fd4c9860d576a812af5
bara baraya selatan <barabarayaselatan96@gmail.com>
2025-08-16 03:19:47 +0000

    rubah warna latar belakang halaman dengan warna biru muda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center bg-background">
+    <div className="relative flex min-h-screen flex-col items-center justify-center bg-blue-100">
       <div className="relative z-10 mx-auto flex w-full max-w-[350px] flex-col justify-center space-y-6 rounded-lg bg-background/80 p-6 shadow-lg backdrop-blur-sm">
         <Image
           src="https://images.unsplash.com/photo-1755310672242-80ce88b024b9"
           alt="Banner"
           width={350}
-          height={88}
+          height={187}
           className="rounded-md object-cover"
           data-ai-hint="nature landscape"
         />
